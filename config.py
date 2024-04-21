716a80d5e8506192f4b5fc5417c2ad1193b5c908
KetoRD <78993548+KetoRD@users.noreply.github.com>
2024-04-21 07:49:17 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,46 @@
+import torch
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
+LEARNING_RATE = 3e-5
+WEIGHT_DECAY = 5e-4
+BATCH_SIZE = 12
+NUM_EPOCHS = 80
+NUM_WORKERS = 3
+CHECKPOINT_FILE = "b3.pth.tar"
+PIN_MEMORY = True
+SAVE_MODEL = True
+LOAD_MODEL = True
+
+train_transforms = A.Compose(
+    [
+        A.Resize(width=760, height=760),
+        A.RandomCrop(height=728, width=728),
+        A.HorizontalFlip(p=0.5),
+        A.VerticalFlip(p=0.5),
+        A.RandomRotate90(p=0.5),
+        A.Blur(p=0.3),
+        A.CLAHE(p=0.3),
+        A.ColorJitter(p=0.3),
+        A.CoarseDropout(max_holes=12, max_height=20, max_width=20, p=0.3),
+        A.IAAAffine(shear=30, rotate=0, p=0.2, mode="constant"),
+        A.Normalize(
+            mean=[0.3199, 0.2240, 0.1609],
+            std=[0.3020, 0.2183, 0.1741],
+            max_pixel_value=255.0,
+        ),
+        ToTensorV2(),
+    ]
+)
+
+val_transforms = A.Compose(
+    [
+        A.Resize(height=728, width=728),
+        A.Normalize(
+            mean=[0.3199, 0.2240, 0.1609],
+            std=[0.3020, 0.2183, 0.1741],
+            max_pixel_value=255.0,
+        ),
+        ToTensorV2(),
+    ]
+)
---
+++ preprocess_dr_images.py
@@ -0,0 +1,57 @@
+import os
+import numpy as np
+from PIL import Image
+import warnings
+from multiprocessing import Pool
+from tqdm import tqdm
+import cv2
+
+
+def trim(im):
+    percentage = 0.03
+
+    img = np.array(im)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    im = img_gray > 0.1 * np.mean(img_gray[img_gray != 0])
+    row_sums = np.sum(im, axis=1)
+    col_sums = np.sum(im, axis=0)
+    rows = np.where(row_sums > img.shape[1] * percentage)[0]
+    cols = np.where(col_sums > img.shape[0] * percentage)[0]
+    min_row, min_col = np.min(rows), np.min(cols)
+    max_row, max_col = np.max(rows), np.max(cols)
+    im_crop = img[min_row : max_row + 1, min_col : max_col + 1]
+    return Image.fromarray(im_crop)
+
+
+def resize_maintain_aspect(image, desired_size):
+    old_size = image.size 
+    ratio = float(desired_size) / max(old_size)
+    new_size = tuple([int(x * ratio) for x in old_size])
+    im = image.resize(new_size, Image.ANTIALIAS)
+    new_im = Image.new("RGB", (desired_size, desired_size))
+    new_im.paste(im, ((desired_size - new_size[0]) // 2, (desired_size - new_size[1]) // 2))
+    return new_im
+
+
+def save_single(args):
+    img_file, input_path_folder, output_path_folder, output_size = args
+    image_original = Image.open(os.path.join(input_path_folder, img_file))
+    image = trim(image_original)
+    image = resize_maintain_aspect(image, desired_size=output_size[0])
+    image.save(os.path.join(output_path_folder + img_file))
+
+
+def fast_image_resize(input_path_folder, output_path_folder, output_size=None):
+    if not output_size:
+        exit()
+
+    if not os.path.exists(output_path_folder):
+        os.makedirs(output_path_folder)
+
+    jobs = [
+        (file, input_path_folder, output_path_folder, output_size)
+        for file in os.listdir(input_path_folder)
+    ]
+
+    with Pool() as p:
+        list(tqdm(p.imap_unordered(save_single, jobs), total=len(jobs)))
